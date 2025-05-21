3501217828ae8d17ce2666253b7cefa54d742538
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2025-05-21 22:47:32 +0200

    set config

---
+++ Dockerfile.php
@@ -0,0 +1,12 @@
+FROM php:8.2-cli
+
+# Installer les extensions nécessaires (dont pdo_mysql)
+RUN apt-get update && apt-get install -y \
+    libpng-dev \
+    libonig-dev \
+    libxml2-dev \
+    zip \
+    unzip \
+    curl \
+    libzip-dev \
+    && docker-php-ext-install pdo pdo_mysql
